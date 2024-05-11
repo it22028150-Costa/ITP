2efc993463245f060c406c977b6d8bafd286d3ac
Sanjana de Costa <sanjana.decosta@itelasoft.com>
2024-05-12 03:15:49 +0530

    Added search and print functions to complaint

--- client/src/components/Complaints/User/MyComplaints/MyComplaints.js
+++ client/src/components/Complaints/User/MyComplaints/MyComplaints.js
@@ -1,111 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useReactToPrint } from "react-to-print";
 
 function MyComplaints() {
   const [complain, setComplain] = useState([]);
-  const [deletecomplain, setdeletecomplain] = useState();
-  
-  // const handleChange = (e) => {
-  //   setGmail(e.target.value);
-  // };
-
-  
+  const componentRef = useRef();
+  const [searchkey,setsearchkey]=useState('');
 
   useEffect(() => {
     const fetchOrders = async () => {
-        
-        
-        try{
-            const response = await axios.get(`http://localhost:3500/complaindb/getcomplaints`,{
-              params: {gmail: localStorage.getItem('currentUser')
-              }});
-            setComplain(response.data);
-            console.log(response.data)
-            
-        }catch(err){
-            console.error(err);
-            alert(err.response.data.message)
-            
-        }
+      try {
+        const response = await axios.get(`http://localhost:3500/complaindb/getcomplaints`, {
+          params: { gmail: localStorage.getItem('currentUser') }
+        });
+        setComplain(response.data);
+        console.log(response.data)
+      } catch (err) {
+        console.error(err);
+        alert(err.response.data.message)
+      }
     };
-    fetchOrders(); 
+    fetchOrders();
+  }, []);
 
-  },[]);
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     //Check Gmail first
-  //     const response = await axios.get(
-  //       `http://localhost:3500/complaindb/_complaint?gmail=${gmail}`
-  //     );
-  //     console.log("Response:", response.data);
-  //     const relevantCard = response.data.complain.filter(
-  //       (complain) => complain.gmail === gmail
-  //     );
-  //     //Display Related Card
-  //     setComplain(relevantCard);
-
-  //     if (relevantCard.length === 0) {
-  //       alert("No  found,Plase enter valid Gmail address");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching ratings:", error);
-  //   }
-  // };
-  /*Delete Code */
   const deleteHandler = async (_id) => {
-    //delete Confirmation
     const confirmed = window.confirm(
       "Are you sure you want to delete this Complain?"
     );
-
-
-
     if (confirmed) {
       try {
-        await axios.delete(`http://localhost:3500/complaindb/delete_complaint`,{
-          params: {id:_id 
-          }});
+        await axios.delete(`http://localhost:3500/complaindb/delete_complaint`, {
+          params: { id: _id }
+        });
         window.alert("Complain deleted successfully!");
         window.location.reload();
       } catch (error) {
-        // Handle deletion error if needed
         console.error("Error deleting Complain details:", error);
       }
     }
   };
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current
+  });
+
+
+      //serach
+      const handlesearch = (e) => {
+
+        filterdata(searchkey);
+    }
+    const filterdata = (searchKey) => {
+        const filteredData = complain.filter(complain =>
+            complain.name.toLowerCase().includes(searchKey.toLowerCase())
+        );
+        setComplain(filteredData);
+    }
+  
+
   return (
     <div className="bk_img">
       <button
-              className="home_btn_set"
-              onClick={() => (window.location.href = "/addcomplain")}
-            >
-              Add Complain
-            </button>
-      <div className="from_box_chek">
-        {/* <form className="from_nw" onSubmit={handleSubmit}>
-          <label className="form_box_item_lable" htmlFor="gmail">
-            Enter Your Gmail
-          </label>
-          <br></br>
-          <input
-            className="form_box_item_input"
-            type="email"
-            id="gmail"
-            name="gmail"
-            value={gmail}
-            onChange={handleChange}
-            required
-          />
-          <br></br>
-          <button className="admin_form_cneter_btn" type="submit">
-            Check
-          </button>
-        </form> */}
-      </div>
-      <div className="tbl_data">
+        className="home_btn_set"
+        onClick={() => (window.location.href = "/addcomplain")}
+      >
+        Add Complain
+      </button>
+      <button className="home_btn_set" onClick={handlePrint}>
+        Print
+      </button>
+      <div class='searchbtnfn'>
+                <input  type="text" onChange={(e)=>setsearchkey(e.target.value)} placeholder='Details..' class='infn'/> <t></t> 
+       
+                <button  id='search-btnfn'  onClick={(e)=>handlesearch(e)}> Search </button>
+            </div>
+
+      <div className="tbl_data" ref={componentRef}>
         <table className="table_details_admin">
           <thead>
             <tr className="tble_card_details_tr">
@@ -119,8 +90,8 @@ function MyComplaints() {
             </tr>
           </thead>
           {complain.map((complain, index) => (
-            <tbody>
-              <tr key={index}>
+            <tbody key={index}>
+              <tr>
                 <td className="admin_tbl_td">{complain.name}</td>
                 <td className="admin_tbl_td">{complain.phone}</td>
                 <td className="admin_tbl_td">{complain.time}</td>
@@ -131,7 +102,7 @@ function MyComplaints() {
                 </td>
                 <td className="admin_tbl_td">
                   <Link
-                    onClick={()=>localStorage.setItem("complainNo",complain._id)}
+                    onClick={() => localStorage.setItem("complainNo", complain._id)}
                     to={`/updatecomplain`}
                     className="booknow_btn"
                   >
